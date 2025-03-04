7a0eb8bda73d597bd14287fb997439310dfdbcb0
vanehru <nehruchowdary@gmail.com>
2025-03-04 12:44:53 +0900

    initial-commit

---
+++ Prod/main.tf
@@ -0,0 +1,15 @@
+#This Terraform Code Deploys Basic VPC Infra.
+provider "aws" {
+  # access_key = "${var.aws_access_key}"
+  # secret_key = "${var.aws_secret_key}"
+  region = var.aws_region
+}
+
+terraform {
+  backend "s3" {
+    bucket = "workspacesbucket01"
+    key    = "Production.statefile"
+    region = "us-east-1"
+    #dynamodb_table = "dynamodb-state-locking"
+  }
+}
